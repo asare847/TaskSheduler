1184710820e49d53b4d24605909366d2cd8f68ba
asare847 <gideonasare847@outlook.com>
2022-02-14 20:27:21 +0000

    first commit

---
+++ app/Console/Commands/MinuteUpdate.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use App\Models\User;
+class MinuteUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    
+    protected $signature = 'minute:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Munite description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $user = User::all();
+        foreach($user as $all){
+            Mail::raw('This is automatically generated mail',function($message) use ($all){
+                $message->from('noreply@gideonasare.net');
+                $message->to($all->email)->subject('Minute Update');
+            });
+           
+        }
+        $this->info('Minute Update has been sent sucessfully');
+    }
+}
